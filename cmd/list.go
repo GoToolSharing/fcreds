be159bc796fd4b1f2adad424e8922d3957cd9bc3
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-06-17 17:20:15 +0200

    Update list command with database data

--- cmd/list.go
+++ cmd/list.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/QU35T-code/fzf-creds/database"
+	"github.com/QU35T-code/fzf-creds/models"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 )
@@ -14,13 +17,17 @@ var listCmd = &cobra.Command{
 	Long:  `Displays the list of tools that work with fzf-creds`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Tools linked to fzf-creds")
+		var tools []models.Tools
+		result := database.DB.Find(&tools)
+		if result.Error != nil {
+			log.Fatal(result.Error)
+		}
 		t := table.NewWriter()
 		t.SetOutputMirror(os.Stdout)
 		t.AppendHeader(table.Row{"Tool"})
-		t.AppendRows([]table.Row{
-			{"smbclient.py"},
-			{"rpcclient.py"},
-		})
+		for _, tool := range tools {
+			t.AppendRows([]table.Row{{tool.Name}})
+		}
 		t.AppendSeparator()
 		t.Render()
 	},
